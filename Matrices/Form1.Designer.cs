729669e5f7d756ce5a0f1b39831f024fc6b29122
Semsema <Semsema@DESKTOP-R6NB9BC>
2025-11-30 21:08:14 +0200

    Add project files.

---
+++ Matrices/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Matrices
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            Matrix1 = new TextBox();
+            Matrix2 = new TextBox();
+            Result = new TextBox();
+            Add = new Button();
+            Subtract = new Button();
+            Multiply = new Button();
+            Inverse1 = new Button();
+            Determinant1 = new Button();
+            Add.Click += Add_Click;
+            Subtract.Click += Subtract_Click;
+            Multiply.Click += Multiply_Click;
+            Determinant1.Click += Determinant1_Click;
+            Inverse1.Click += Inverse1_Click;
+            SuspendLayout();
+            // Matrix1
+            Matrix1.BackColor = SystemColors.InactiveCaption;
+            Matrix1.Location = new Point(0, 2);
+            Matrix1.Multiline = true;
+            Matrix1.Name = "Matrix1";
+            Matrix1.Size = new Size(517, 205);
+            Matrix1.TabIndex = 0;
+            Matrix1.TextChanged += Matrix1_TextChanged;
+            // Matrix2
+            Matrix2.BackColor = SystemColors.InactiveCaption;
+            Matrix2.Location = new Point(786, 2);
+            Matrix2.Multiline = true;
+            Matrix2.Name = "Matrix2";
+            Matrix2.Size = new Size(517, 205);
+            Matrix2.TabIndex = 1;
+            // Result
+            Result.BackColor = SystemColors.InactiveCaption;
+            Result.Location = new Point(0, 518);
+            Result.Multiline = true;
+            Result.Name = "Result";
+            Result.Size = new Size(1303, 205);
+            Result.TabIndex = 2;
+            // Add
+            Add.BackColor = SystemColors.ControlDarkDark;
+            Add.ForeColor = SystemColors.Control;
+            Add.Location = new Point(0, 259);
+            Add.Name = "Add";
+            Add.Size = new Size(167, 87);
+            Add.TabIndex = 3;
+            Add.Text = "Add";
+            Add.UseVisualStyleBackColor = false;
+            // Subtract
+            Subtract.BackColor = SystemColors.ControlDarkDark;
+            Subtract.ForeColor = SystemColors.Control;
+            Subtract.Location = new Point(567, 259);
+            Subtract.Name = "Subtract";
+            Subtract.Size = new Size(167, 87);
+            Subtract.TabIndex = 4;
+            Subtract.Text = "Subtract";
+            Subtract.UseVisualStyleBackColor = false;
+            // Multiply
+            Multiply.BackColor = SystemColors.ControlDarkDark;
+            Multiply.ForeColor = SystemColors.Control;
+            Multiply.Location = new Point(1136, 259);
+            Multiply.Name = "Multiply";
+            Multiply.Size = new Size(167, 87);
+            Multiply.TabIndex = 5;
+            Multiply.Text = "Multiply";
+            Multiply.UseVisualStyleBackColor = false;
+            // Inverse1
+            Inverse1.BackColor = SystemColors.ControlDarkDark;
+            Inverse1.ForeColor = SystemColors.Control;
+            Inverse1.Location = new Point(567, 384);
+            Inverse1.Name = "Inverse1";
+            Inverse1.Size = new Size(167, 87);
+            Inverse1.TabIndex = 6;
+            Inverse1.Text = "Inverse1";
+            Inverse1.UseVisualStyleBackColor = false;
+            // Determinant1
+            Determinant1.BackColor = SystemColors.ControlDarkDark;
+            Determinant1.ForeColor = SystemColors.Control;
+            Determinant1.Location = new Point(0, 384);
+            Determinant1.Name = "Determinant1";
+            Determinant1.Size = new Size(167, 87);
+            Determinant1.TabIndex = 7;
+            Determinant1.Text = "Determinant1";
+            Determinant1.UseVisualStyleBackColor = false;
+            // Form1
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1409, 726);
+            Controls.Add(Determinant1);
+            Controls.Add(Inverse1);
+            Controls.Add(Multiply);
+            Controls.Add(Subtract);
+            Controls.Add(Add);
+            Controls.Add(Result);
+            Controls.Add(Matrix2);
+            Controls.Add(Matrix1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+        #endregion
+        private TextBox Matrix1;
+        private TextBox Matrix2;
+        private TextBox Result;
+        private Button Add;
+        private Button Subtract;
+        private Button Multiply;
+        private Button Inverse1;
+        private Button Determinant1;
+        private void Matrix1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Matrices/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Matrices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double[,] ParseMatrix(string text)
+        {
+            var rows = text.Trim().Split('\n');
+            int r = rows.Length;
+            int c = rows[0].Trim().Split(' ').Length;
+
+            double[,] m = new double[r, c];
+
+            for (int i = 0; i < r; i++)
+            {
+                var nums = rows[i].Trim().Split(' ');
+                for (int j = 0; j < c; j++)
+                    m[i, j] = double.Parse(nums[j]);
+            }
+            return m;
+        }
+        // Convert the Matrix to Text
+        string MatrixToString(double[,] m)
+        {
+            string result = "";
+            int r = m.GetLength(0);
+            int c = m.GetLength(1);
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                    result += m[i, j] + " ";
+                result += Environment.NewLine;
+            }
+            return result;
+        }
+        // Combine button
+        private void Add_Click(object sender, EventArgs e)
+        {
+            var A = ParseMatrix(Matrix1.Text);
+            var B = ParseMatrix(Matrix2.Text);
+            int r = A.GetLength(0);
+            int c = A.GetLength(1);
+            double[,] R = new double[r, c];
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < c; j++)
+                    R[i, j] = A[i, j] + B[i, j];
+            Result.Text = MatrixToString(R);
+        }
+        // Subtract button
+        private void Subtract_Click(object sender, EventArgs e)
+        {
+            var A = ParseMatrix(Matrix1.Text);
+            var B = ParseMatrix(Matrix2.Text);
+            int r = A.GetLength(0);
+            int c = A.GetLength(1);
+            double[,] R = new double[r, c];
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < c; j++)
+                    R[i, j] = A[i, j] - B[i, j];
+            Result.Text = MatrixToString(R);
+        }
+        // Multiplication button
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            var A = ParseMatrix(Matrix1.Text);
+            var B = ParseMatrix(Matrix2.Text);
+            int r1 = A.GetLength(0);
+            int c1 = A.GetLength(1);
+            int r2 = B.GetLength(0);
+            int c2 = B.GetLength(1);
+            if (c1 != r2)
+            {
+                MessageBox.Show("عدد أعمدة Matrix1 يجب يساوي عدد صفوف Matrix2");
+                return;
+            }
+            double[,] R = new double[r1, c2];
+            for (int i = 0; i < r1; i++)
+                for (int j = 0; j < c2; j++)
+                    for (int k = 0; k < c1; k++)
+                        R[i, j] += A[i, k] * B[k, j];
+            Result.Text = MatrixToString(R);
+        }
+        // Determinant
+        private double Determinant(double[,] m)
+        {
+            int n = m.GetLength(0);
+            if (n == 1) return m[0, 0];
+            if (n == 2) return m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0];
+            double det = 0;
+            for (int k = 0; k < n; k++)
+            {
+                double[,] sub = new double[n - 1, n - 1];
+                for (int i = 1; i < n; i++)
+                {
+                    int colIndex = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (j == k) continue;
+                        sub[i - 1, colIndex++] = m[i, j];
+                    }
+                }
+                det += Math.Pow(-1, k) * m[0, k] * Determinant(sub);
+            }
+            return det;
+        }
+        private void Determinant1_Click(object sender, EventArgs e)
+        {
+            var A = ParseMatrix(Matrix1.Text);
+            double det = Determinant(A);
+            Result.Text = "Determinant = " + det;
+        }
+        // Inverse Matrix
+        private double[,] Adjoint(double[,] m)
+        {
+            int n = m.GetLength(0);
+            double[,] adj = new double[n, n];
+            if (n == 1)
+            {
+                adj[0, 0] = 1;
+                return adj;
+            }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    double[,] sub = new double[n - 1, n - 1];
+                    int r = 0, c = 0;
+                    for (int x = 0; x < n; x++)
+                    {
+                        if (x == i) continue;
+                        c = 0;
+                        for (int y = 0; y < n; y++)
+                        {
+                            if (y == j) continue;
+                            sub[r, c++] = m[x, y];
+                        }
+                        r++;
+                    }
+                    adj[j, i] = Math.Pow(-1, i + j) * Determinant(sub);
+                }
+            return adj;
+        }
+        private void Inverse1_Click(object sender, EventArgs e)
+        {
+            var A = ParseMatrix(Matrix1.Text);
+            double det = Determinant(A);
+            double epsilon = 1e-10; // قيمة صغيرة جدًا
+
+            if (Math.Abs(det) < epsilon)
+            {
+                MessageBox.Show("المصفوفة غير قابلة للعكس!");
+                return;
+            }
+            double[,] adj = Adjoint(A);
+            int n = A.GetLength(0);
+            double[,] inv = new double[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    inv[i, j] = adj[i, j] / det;
+
+            Result.Text = MatrixToString(inv);
+        }
+    }
+}
